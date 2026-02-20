40a74dbbe485f92b4207824fa145f283d6e0fbcd
haohan-xu-hue <hxu643@gatech.edu>
2026-02-20 17:42:50 -0500

    Update Home_Page.py

--- Lab1_WebDev_Xu/Home_Page.py
+++ Lab1_WebDev_Xu/Home_Page.py
@@ -1,28 +1,16 @@
 import streamlit as st
 
-# Title of App
 st.title("Web Development Lab01")
-
-# Assignment Data 
-# TODO: Fill out your team number, section, and team members
 
 st.header("CS 1301")
 st.subheader("Web Development - Section Xu")
 st.subheader("Haohan Xu")
 
-
-# Introduction
-# TODO: Write a quick description for all of your pages in this lab below, in the form:
-#       1. **Portfolio**: Learn more about my education, skills, and experience.
-#       2. **Quiz**: Take a fun interactive quiz about XYZ!
-#       3. **Page Name**: Description
-#       4. **Page Name**: Description
-
 st.write("""
 Welcome to our Streamlit Web Development Lab01 app! You can navigate between the pages using the sidebar to the left. The following pages are:
 
-1. Portfolio
-2. Quiz
+1. Portfolio： Learn more about my education, skills, and experience.
+2. Quiz：Which kind of travel you prefer - urban or natural？
 3.
 4.
 
